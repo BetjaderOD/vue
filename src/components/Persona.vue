06ed775b955e7d22788ec7802a446f7765a395a7
BetjaderOD <20213tn032@utez.edu.mx>
2024-02-03 19:17:55 -0600

    ctx

--- src/components/Persona.vue
+++ src/components/Persona.vue
@@ -1,59 +1,62 @@
 <template>
-    <div class="overflow-auto">
-      <b-pagination
-        v-model="currentPage"
-        :total-rows="rows"
-        :per-page="perPage"
-        aria-controls="my-table"
-      ></b-pagination>
+  <div class="overflow-auto">
+    <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage" aria-controls="my-table"></b-pagination>
+
+    <p class="mt-3">Current Page: {{ currentPage }}</p>
+
+    <b-table id="my-table" :items="personas" :per-page="perPage" :current-page="currentPage" :fields="fields"
+      :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" label-sort-asc="" label-sort-desc="" :filter="filtro"
+      @filtered="onFiltered" small></b-table>
+  </div>
+</template>
   
-      <p class="mt-3">Current Page: {{ currentPage }}</p>
-  
-      <b-table
-        id="my-table"
-        :items="personas"
-        :per-page="perPage"
-        :current-page="currentPage"
-        small
-      ></b-table>
-    </div>
-  </template>
-  
-  <script>
+<script>
 import PersonaAxios from './services/PersonaAxios';
 
-    export default {
-      data() {
-        return {
-          perPage: 3,
-          currentPage: 1,
-         personas:[],
-
-        }
-      },
-      mounted(){ 
-        this.obtenerPersonas()
-      },
-      methods:{
-        async obtenerPersonas(){
-            try {
-                const data = await PersonaAxios.obtenerPersonasPaginadas(
-                    parseInt(this.currentPage),
-                    parseInt(this.perPage)
-                );
-                // const personasData = data.content
-                // console.log(personasData)
-                this.personas = data.content;
-                console.log(this.personas)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-      },
-      computed: {
-        rows() {
-          return this.personas.length
-        }
+export default {
+  data() {
+    return {
+      filtro: null,
+      sortBy: "name",
+      sortDesc: false,
+      perPage: 5,
+      rows: 0,
+      currentPage: 1,
+      personas: [],
+      fields: [
+        { key: "name", label: "Nombre", sortable: true },
+        { key: "firstname", label: "Apellido Paterno", sortable: true },
+      ],
+    };
+  },
+  mounted() {
+    this.obtenerPersonas()
+  },
+  methods: {
+    async obtenerPersonas() {
+      try {
+        const data = await PersonaAxios.obtenerPersonasPaginadas(
+          parseInt(ctx.currentPage) - 1,
+          parseInt(ctx.perPage),
+          ctx.sortBy
+        );
+        // const personasData = data.content
+        // console.log(personasData)
+        this.rows = data.totalElements;
+        return data.content;
+      } catch (error) {
+        console.log(error)
       }
     }
-  </script>
+  },
+  onFiltered(filteredItems) {
+    this.currentPage = 1;
+    this.rows = filteredItems.length;
+  },
+  computed: {
+    rows() {
+      return this.personas.length
+    }
+  }
+}
+</script>
--- src/components/services/PersonaAxios.js
+++ src/components/services/PersonaAxios.js
@@ -3,11 +3,13 @@ import axios from "axios";
 
 const API_URL = 'http://localhost:8080/api/personas/page';
 
-const obtenerPersonasPaginadas = async (page, size) => {
+const obtenerPersonasPaginadas = async (page, size, sort) => {
+  const params = {
+    page,
+    size, sort
+  };
   try {
-    const response = await axios.post(API_URL, {
-      params: { page, size }
-    });
+    const response = await axios.post(API_URL, null, { params });
 
     return response.data;
   } catch (error) {
