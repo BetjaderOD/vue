aab73c25dc8cce0c57cbe9b430ad2f022847c4d1
BetjaderOD <20213tn032@utez.edu.mx>
2024-02-19 19:29:13 -0600

    Chapcha

--- src/components/jugetes/Electronicos.vue
+++ src/components/jugetes/Electronicos.vue
@@ -3,14 +3,47 @@
     <b-breadcrumb :items="items"></b-breadcrumb>
 
     <h1>Electronicos</h1>
+    <form @submit.prevent="submitForm">
+      <label for="name">
+        Nombre:
+      </label>
+      <br>
+      <input
+      type="text"
+      id="name"
+        v-model="form.name"
+            required
+       >
+
+       <br>
+       <div class="frc-captcha"
+       ref="container"
+   data-sitekey="FCMJFL85J8RD5KAN"
+   data-lang="es"    
+   >
+   
+       </div>
+       <br>
+       <b-button  type="submit">Enviar</b-button>
+    </form>
+    <br>
     <b-button to="/juegos">Juegos</b-button>
   </div>
 </template>
 
 <script>
+import {WidgetInstance} from "friendly-challenge";
+import {ref} from "vue"
+
 export default {
   data() {
     return {
+      container:ref("container"),
+      widget:ref(),
+      form:{
+        name:"",
+        capchaToken:null
+      },
       items: [
         {
           text: "Inicio",
@@ -32,5 +65,32 @@ export default {
       ],
     };
   },
+    methods:{
+      submitForm: () => {},
+     doneCallback: (solution) => {
+    console.log('Captcha was solved. The form can be submitted.');
+    console.log(solution);
+  },
+
+ errorCallback : (err) => {
+  console.log('There was an error when trying to solve the Captcha.');
+  console.log(err);
+}
+    
+  },
+  mounted (){
+    if (this.$refs.container){
+      this.widget = new WidgetInstance(this.$refs.container ,{
+        startMode: "auto",
+        doneCallback : this.doneCallback,  
+      errorCallback : this.errorCallback
+      })
+    }
+  },
+  beforeDestroy() {
+    if (this.widget){
+      this.widget.destroy();
+    }
+  }
 }
 </script>
