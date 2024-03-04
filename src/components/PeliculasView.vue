0561ff132089800280ad657f644788dbe16a5a56
BetjaderOD <20213tn032@utez.edu.mx>
2024-03-04 09:50:37 -0600

    Se comento el codigo

--- src/components/PeliculasView.vue
+++ src/components/PeliculasView.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="Peliculas">
+
+  <!-- Animacion  llamando a la funcion  -->
     <h1 ref="heading" @click="applyBounceAnimation">Peliculas <span style="color: red; font-weight: 500;">
         ¡¡¡PRESIONAME!!!</span></h1>
 
@@ -54,6 +56,7 @@
             required />
           <br>
           <label class="mx-1" for="categorias">Selecciona la categoria</label>
+          <!-- Cargar los elementos de categorias -->
           <b-form-select id="categorias" v-model="peliculaSeleccionada.categoria.id" :options="form.categorias"
             @change="onCategoriaChange"></b-form-select>
 
@@ -67,9 +70,22 @@
     <!-- Formulario arrastrar -->
     <div class="row">
 
-      <div class="drop-zone-1 col " @drop="onDrop($event)" @dragstart="startDrag($event)">
-        <div v-show="showElement" class="drag-el" style="background-color: rgb(179, 179, 247);" draggable
-          @dragstart="startDrag($event)">
+      <!-- 
+    Esta es una zona de suelta donde puedes arrastrar elementos. 
+    Al soltar un elemento aquí, se ejecutará la función 'onDrop($event)'.
+-->
+
+ <div class="drop-zone-1 col " @drop="onDrop($event)" @dragstart="startDrag($event)">
+    
+    <!-- 
+        Este div es el elemento que se mostrará cuando 'showElement' sea verdadero.
+        Tiene la clase 'drag-el' para aplicar estilos de arrastre.
+        El atributo 'draggable' permite que este elemento sea arrastrable.
+        Cuando se inicia el arrastre, se ejecuta la función 'startDrag($event)'.
+    -->
+    <div v-show="showElement" class="drag-el" style="background-color: rgb(179, 179, 247);" draggable
+        @dragstart="startDrag($event)">
+
           <div class="container text-center mt-5">
             <h1>Formulario</h1>
             <b-form @submit="onSubmit" @reset="onReset" v-if="show">
@@ -88,6 +104,7 @@
                 <b-form-input id="input-4" v-model="form.duracion" type="text" placeholder="Ingresa duracion" required />
                 <br>
                 <label class="mx-1" for="categorias">Selecciona la categoria</label>
+                   <!-- Identificador del componente, Valor seleccionado vinculado a una propiedad, Lista de opciones disponibles  -->
                 <b-form-select id="categorias" v-model="form.selected" :options="form.categorias"></b-form-select>
                 <br>
                 <br>
@@ -101,7 +118,11 @@
         </div>
       </div>
 
-
+<!-- 
+  
+    Cuando se produce un evento de soltar, se activa la función 'onDrop($event)'.
+    Los eventos 'dragover' y 'dragenter' se previenen por defecto para permitir soltar elementos.
+-->
       <div class=" col drop-zone" @drop="onDrop($event)" @dragover.prevent @dragenter.prevent>
         <h4>Suelta el formulario aqui para realizar en envio o usa el boton "Guardar"</h4>
       </div>
@@ -109,6 +130,9 @@
 
     <div>
       <b-row>
+       <!-- 
+    Utilizando 'v-for' para iterar sobre el array 'peliculas'.
+    ':key="pelicula.id"' asegura que cada elemento iterado tenga una clave única basada en su ID -->
         <b-col cols="12" md="4" v-for="(pelicula, index) in peliculas" :key="pelicula.id">
           <b-card no-body style="max-width: 20rem;" class="mb-3">
             <img :src="pelicula.enlace" class="card-img-top" alt="...">
@@ -135,9 +159,11 @@ import { bounce } from '@asika32764/vue-animate';
 export default {
   data() {
     return {
+      // Variable para mostrar u ocultar el scroll 
       showElement: true,
-
+// Variable para guardar la posicion del scroll
       lastScrollPosition: 0,
+      // Formulario para agregar una película donde se guardan los datos y las categorias
       form: {
         titulo: '',
         director: '',
@@ -146,9 +172,12 @@ export default {
         selected: null,
         categorias: [],
       },
+      // Variable para mostrar el error del formulario
       show: true,
       formError: '',
+      // Variable para guardar las películas
       peliculas: [],
+      // Variable para guardar la película seleccionada para editar
       peliculaSeleccionada: {
         id: '',
         titulo: '',
@@ -159,17 +188,24 @@ export default {
       }
     }
   },
+  // Metodo para obtener las peliculas y las categorias
   mounted() {
+    // Llamada a la funcion obtenerPeliculas
     this.obtenerPeliculas();
+    // Llamada a la funcion obtenerCategorias
     this.obtenerCategorias();
+    // Evento para el scroll
     window.addEventListener("scroll", this.onScroll);
 
   },
+  // Metodo para eliminar el evento del scroll
   beforeDestroy() {
+    // Elimina el evento del scroll
     window.removeEventListener("scroll", this.onScroll);
   },
+  // Metodo para aplicar la animacion
   methods: {
-
+// Metodo para aplicar la animacion
     applyBounceAnimation() {
       // Accede al elemento h1 utilizando refs
       const element = this.$refs.heading;
@@ -177,67 +213,96 @@ export default {
       // Aplica la animación de "bounce" al elemento h1
       bounce(element);
     },
+
+    // Metodo para el evento de soltar
     onDrop(event) {
       console.log('Soltado');
       console.log(event);
+      // Llamada a la funcion onSubmit
       this.onSubmit();
 
     },
-
+// Metodo para el evento de arrastrar
     startDrag(event) {
       console.log('Drag started');
       console.log(event);
     },
+
+    //metodo para obtener las peliculas
     async obtenerPeliculas() {
+
       try {
+        // Llamada a la funcion obtenerPeliculas de peliculasAxios
         const data = await peliculasAxios.obtenerPeliculas();
+        // Guarda las peliculas en la variable peliculas
         this.peliculas = data.object;
       } catch (error) {
         console.error('Error al obtener las películas', error);
       }
     },
+    // Metodo para el onScroll
     onScroll() {
-
+// Variable para guardar la posicion del scroll 
       const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
       console.log(currentScrollPosition);
-
+// Si la diferencia entre la posicion actual y la ultima posicion es menor a 20
       if (Math.abs(currentScrollPosition - this.lastScrollPosition) < 20) {
+        // No hace nada
         return;
       }
+// Si la posicion actual es menor a la ultima posicion
       this.showElement = currentScrollPosition < this.lastScrollPosition;
+// Guarda la posicion actual en la ultima posicion
       this.lastScrollPosition = currentScrollPosition;
     },
+    // Metodo para obtener las categorias
     async obtenerCategorias() {
       try {
+        // Llamada a la funcion obtenerCategorias de peliculasAxios
         const data = await peliculasAxios.obtenerCategorias();
+        // Guarda las categorias en la variable categorias y las mapea por su id y nombre
         this.form.categorias = data.object.map(categoria => ({ value: categoria.id, text: categoria.nombre }));
       } catch (error) {
         console.error('Error al obtener las categorias', error);
       }
     },
+    // Metodo para eliminar una pelicula a traves de su id
     async eliminarPelicula(id) {
+
       try {
+        // Llamada a la funcion eliminarPelicula de peliculasAxios
         await peliculasAxios.eliminarPelicula(id);
+        // Llamada a la funcion obtenerPeliculas para actualizar la lista de peliculas
         this.obtenerPeliculas();
+
         alert('Película eliminada correctamente');
       } catch (error) {
         console.error('Error al eliminar la película', error);
         alert('Error al eliminar la película');
       }
     },
+    // Metodo para abrir el modal de editar pelicula
     abrirModalEditar(pelicula) {
+      // Guarda la pelicula seleccionada en la variable peliculaSeleccionada
       this.peliculaSeleccionada = { ...pelicula };
+      // Muestra el modal de editar
       this.$bvModal.show('modal-editar');
 
     },
+    // Metodo para editar una pelicula a la que se le pasa el evento para 
     async editarPelicula(event) {
+      //Permite prevenir el comportamiento por defecto de un evento 
       event.preventDefault();
-
+// Si el formulario no es valido
       if (!this.validateForm()) {
+        // No hace nada
         return;
+
       }
       try {
+        // Llamada a la funcion editarPelicula de peliculasAxios
         await peliculasAxios.editarPelicula(
+          // Se le pasa el id, titulo, director, duracion, enlace y categoria de la pelicula seleccionada
           this.peliculaSeleccionada.id,
           this.peliculaSeleccionada.titulo,
           this.peliculaSeleccionada.director,
@@ -246,23 +311,30 @@ export default {
           this.peliculaSeleccionada.categoria.id
         );
         alert('Película editada correctamente');
+        // Llamada a la funcion obtenerPeliculas para actualizar la lista de peliculas
         this.obtenerPeliculas();
+        // Oculta el modal de editar
         this.$bvModal.hide('modal-1');
       } catch (error) {
         console.error('Error al editar la película', error);
         alert('Error al editar la película');
       }
-
+      // Resetea el formulario
       this.resetForm();
     },
+    // Metodo para validar el formulario
     async onSubmit(event) {
       // event.preventDefault();
-
+      // Si el formulario no es valido
       if (!this.validateForm()) {
+        // No hace nada
         return;
       }
+      // Llamada a la funcion crearPelicula de peliculasAxios
       try {
+        // Se le pasa el titulo, director, duracion, enlace y categoria del formulario
         await peliculasAxios.crearPelicula(this.form.titulo, this.form.director, this.form.duracion, this.form.enlace, this.form.selected);
+
         alert('Formulario enviado correctamente');
         this.obtenerPeliculas();
       } catch (error) {
@@ -272,7 +344,9 @@ export default {
 
       this.resetForm();
     },
+    // Metodo para validar el formulario
     validateForm() {
+      
       this.formError = '';
       return true;
     },
@@ -281,6 +355,7 @@ export default {
       this.resetForm();
     },
     resetForm() {
+      // Resetea el formulario
       this.form = {
         titulo: '',
         director: '',
